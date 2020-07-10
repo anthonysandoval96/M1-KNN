326e8f83c862272c0913f906f4df40292c406b92
Venom <anthonysandoval1596@gmail.com>
2020-07-09 21:49:48 -0500

    Caminar de origen a destino calculando las probabilidades

--- class.js
+++ class.js
@@ -1,121 +1,119 @@
 class Persona {
-    // Creamos el constructor de la clase
+    /* Creamos el constructor de la clase */
     constructor(_x = null, _y = null) {
-        // Definimos las propiedades de la clase
-        this.distEuclidiana = 0
-        this.distManhattan = 0
-        // Sí se instancia con paramétros
+        /* Definimos las propiedades de la clase */
+        this.distEuclidiana = 0;
+        this.distManhattan = 0;
+        /* Sí se instancia con paramétros */
         if (_x != null || _y != null) {
             this.x = _x;
             this.y = _y;
             this.estado = true;
             this.color = "blue";
-        } else { // Si se instacia la clase sin parámetros
-             // Creamos posiciones aleatorias dentro del canvas
-            this.x = Math.floor(Math.random() * 1450) + 25;
-            this.y = Math.floor(Math.random() * 700) + 25;
-            // Sí es igual a 0, es una persona infectada
+        } else { /* Si se instacia la clase sin parámetros */
+            /* Creamos posiciones aleatorias dentro del canvas */
+            this.x = Math.floor(Math.random() * 950) + 25;
+            this.y = Math.floor(Math.random() * 650) + 25;
+            /* Sí es igual a 0, es una persona infectada */
             if (Math.floor(Math.random() * 2) % 2 == 0) {
                 this.estado = false;
                 this.color = "#FF0000";
-            } else { // Es una persona sana
+            } else { /* Es una persona sana */
                 this.estado = true;
                 this.color = "#4CAF50";
             }
         }
     }
-    /* Creamos el método graficar, que nos servirá para poder graficar 
-    las personas infectas y sanas. También graficar el jugador */
+    /* Creamos el método graficar, solo para graficar el jugador */
     graficar(context, x = null, y = null) {
+        /* Graficamos el contexto */
         context.beginPath();
-        // Le pasamos un color
-        context.fillStyle = this.color;
-        // Le pasamos las coordenadas
+        /* Le pasamos las coordenadas */
         context.arc(this.x, this.y, 20, 0, 2 * Math.PI);
-        // Colocamos un texto al jugador
+        /* Le pasamos un color */
+        context.fillStyle = this.color;
+        /* Le colocamos un texto */
         context.strokeText("JUGADOR", this.x - 23, this.y + 40);
-        context.closePath();        
-        context.stroke();
         context.fill();
+        context.closePath();
     }
     /* Creamos el método calcDistancia, que nos servirá 
     para poder calcular la distancia euclidiana y manhattan */
     calcDistancia(x, y) {
-        this.distEuclidiana = Math.sqrt((this.x - x) * (this.x - x)+ (this.y - y) * (this.y - y));
-        this.distManhattan = Math.abs(this.x - x)  + Math.abs(this.y - y);
-    }
-    /* Creamos el método setPosicion, que 
-    nos servirá para poder pasarle las coordenadas,
-    cuando movemos el mouse */
-    setPosicion(_x, _y) {
-        this.x = _x;
-        this.y = _y;
+        this.distEuclidiana = Math.sqrt((this.x - x) * (this.x - x) + (this.y - y) * (this.y - y));
+        this.distManhattan = Math.abs(this.x - x) + Math.abs(this.y - y);
     }
 }
 
 class Personas {
-    // Creamos el constructor de la clase
-    constructor() {
-        // Definimos las propiedades de la clase
-        this.nro_personas = 15;
-        this.k = 4;
+    /* Creamos el constructor de la clase */
+    constructor(_nro_personas, _k) {
+        this.nro_personas = _nro_personas;
+        this.k = _k;
         this.personas = [];
         this.simular();
     }
     /* Creamos el método simular */
     simular() {
         document.getElementById("alumno").innerHTML = "<b>MOSQUERA SANDOVAL ANTHONY B.</b>";
-        /* Insertamos todas las personas creadas aleatoriamente al arreglo personas */
         for (var i = 0; i < this.nro_personas; i++) {
             this.personas[i] = new Persona();
         }
     }
-    /* Creamos el método graficarTodos */
+    /* Creamos el método graficarTodos, para poder 
+    graficar todas las personas sanas e infectadas */
     graficarTodos(context, p) {
+        /* Recorremos todas las personas y calculamos sus distancias */
+        for (var i = 0; i < this.nro_personas; i++) {
+            this.personas[i].calcDistancia(p.x, p.y);
+        }
+        /* Ordenamos ascendentemente el arreglo persnas */
+        this.personas.sort(function (a, b) { 
+            return a.distEuclidiana - b.distEuclidiana; 
+        });
         /* Recorremos todas las personas y las graficamos en el canvas */
         for (var i = 0; i < this.nro_personas; i++) {
-            // Utilizamos el método calcDistancia de la clase Personas
-            this.personas[i].calcDistancia(p.x, p.y);
             context.beginPath();
-            // Le pasamos un color
             context.fillStyle = this.personas[i].color;
-            // Le pasamos las coordenadas
+            /* Dibujamos las personas en las posiciones aleatorias */
             context.arc(this.personas[i].x, this.personas[i].y, 20, 0, 2 * Math.PI);
-            // Solo se pinta las líneas con los k vecinos más cercanos
+            /* Enlazamos solos los k vecinos más cercanos */
             if (i < this.k) {
+                /* Para pintar las líneas con el jugador */
                 context.moveTo(this.personas[i].x, this.personas[i].y);
                 context.lineTo(p.x, p.y);
+                /* Para pintar las distancias */
                 context.strokeText("EU: " + this.personas[i].distEuclidiana + " -- " + "MT: " + this.personas[i].distManhattan, this.personas[i].x - 50, this.personas[i].y + 35);
             }
-            context.closePath();        
             context.stroke();
             context.fill();
+            context.closePath();
         }
-        // Utilizamos el método calcularPorcentaje de la misma clase
-        this.calcularPorcentaje();
     }
     /* Creamos el método calcularPorcentaje */
     calcularPorcentaje() {
         /* Definimos las variables a utilizar */
         var contI = 0;
+        var contS = 0;
         var porcentaje = 0;
-        /* Ordenamos ascendentemente el arreglo persnas */
-        this.personas.sort(function(a, b) { 
-            return (a.distEuclidiana - b.distEuclidiana) 
-        });
-        /* Contabilizamos las personas infectadas 
-        de acuerdo a los k vecinos más cercanos */
-        for (var i = 0; i < this.k; i++) { 
-            if (!this.personas[i].estado) {
-                contI++; 
+        /* Contabilizamos las personas infectadas y 
+        sanas de acuerdo a los k vecinos más cercanos */
+        for (var i = 0; i < this.k; i++) {
+            if (this.personas[i].estado) {
+                contS++; 
+            } else {
+                contI++;
             }
         }
         /* Calculamos el porcentaje */
-        porcentaje = parseFloat(100 * contI / this.k).toFixed(2);
-        /* Retornamos el valor del porcentaje y del k utilizado */
+        porcentaje = parseFloat((100 * contI) / this.k).toFixed(2);
+        /* Retornamos el valor del porcentaje, del k utilizado, 
+        el número de infectados y el número de sanos */
         return { 
             porcentaje: porcentaje, 
-            k: this.k 
+            k: this.k, nro_personas: this.nro_personas, 
+            nro_infectados: contI, 
+            nro_sanos: contS, 
         };
     }
 }
--- main.js
+++ main.js
@@ -1,66 +1,104 @@
+/* Definimos el límite de personas a graficar */
+var limit_personas = 50;
 // Referencia al canvas en el html
 var canvas = document.getElementById("myCanvas");
-// Validamos si el canvas existe
-if (canvas && canvas.getContext) {
-    // Inicializamos el contexto del canvas
-    var context = canvas.getContext("2d");
-    // Validamos si el contexto existe
-    if (context) {
-        // Instanciamos la clase Personas
-        var personas = new Personas();
-        // Creamos el evento mousemove
-        canvas.addEventListener(
-            "mousemove",
-            function (evt) {
-                // Instanciamos la clase Persona
-                var jugador = new Persona(0, 0);
-                // Creamos la funcion oMousePos y la asignamos a la variable mousePos
-                var mousePos = oMousePos(canvas, evt);
-                // Utilizamos el método setPosicion de la clase Persona
-                jugador.setPosicion(mousePos.x, mousePos.y);
-                // Limpiamos el contexto del canvas
-                context.clearRect(0, 0, canvas.width, canvas.height);
-                // Utilizamos el método graficarTodos de la clase Personas
-                personas.graficarTodos(context, jugador);
-                // Utilizamos el método graficar de la clase Persona
-                jugador.graficar(context);
-                /* Utilizamos el método calcularPorcentaje de la clase Personas
-                Y se le asigna a la variable result */
-                var result = personas.calcularPorcentaje();
-                // Insertamos html en elemento output
-                document.getElementById("output").innerHTML =
-                    `<h2>Porcentaje de Infección: <span style="color: red;">` +
-                    result.porcentaje +
-                    `%</span>  - <span>Cuando k = ` +
-                    result.k +
-                    `</span>
-          </h2>`;
-            },
-            false
-        );
-        // Creamos el evento submit del formulario form_set_k
-        document.getElementById("form_set_k").addEventListener("submit", form_set_k);
+/* Definimos los ptos de inicio y final */
+var pto_inicio = {x: 25, y: 25};
+var pto_final = {x: canvas.width - pto_inicio.x, y: canvas.height - pto_inicio.y};
+/* Inicializamos las variables */
+var result_data, set_interval;
+/* Llamamos a la función load, 
+ni bien cargue la página */
+load();
+/* Creamos la función load */ 
+function load(nro_personas = 15, k = 3) {
+    /* Validamos si el canvas existe */
+    if (canvas && canvas.getContext) {
+        /* Inicializamos el contexto del canvas */
+        var context = canvas.getContext("2d");
+        /* Limpiamos el contexto del canvas */
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        /* Instanciamos la clase Personas */
+        var personas = new Personas(nro_personas, k);
+        /* Instanciamos la clase Persona */
+        var jugador = new Persona(pto_inicio.x, pto_inicio.y);
+        /* Limpiamos el Interval */
+        clearInterval(set_interval);
+        /* Llamamos al seInterval para que
+        se ejecute cada medio segundo la función route */
+        set_interval = setInterval(function() { 
+            route(context, personas, jugador) 
+        }, 50);
+        /* Creamos el evento submit para el form update */
+        document.getElementById("update").addEventListener("submit", update);
+        /* Creamos el evento click para el button refresh */
+        document.getElementById("refresh").addEventListener("click", refresh);
     }
 }
-/* Creamos la función form_set_k */
-function form_set_k(evt) {
-    evt.preventDefault();
-    var string, value = "";
-    string = $(this).serialize().split("=");
-    value = string[1];
-    personas.k = value;
-    $(this).find("input[name='value_k']").val("");
-    alert("Se cambio el valor de K con éxito");
+/* Creamos la función route, que se ejecutará cada n segundos
+para que el jugador pueda avanzar solo */
+function route(context, personas, jugador) {
+    /* Limpiamos el contexto del canvas */
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    /* Utilizamos el método graficarTodos de la clase Personas */
+    personas.graficarTodos(context, jugador);
+    /* Utilizamos el método graficar de la clase Persona */
+    jugador.graficar(context);
+    /* Utilizamos el método calcularPorcentaje de la clase Personas
+    Y se le asigna a la variable result_data */
+    result_data = personas.calcularPorcentaje();
+    html_data(result_data);
+    /* Sí máximo del ancho en x es menor 
+    a la posición del jugador en x */
+    if (pto_final.x > jugador.x) {
+        /* Avanzar hacia la derecha, 5 posiciones */
+        jugador.x += 5;
+    } else if (pto_final.y > jugador.y) { /* Sí máximo del ancho en y es menor a la posición del jugador en y */
+        /* Avanzar hacia la abajo, 5 posiciones */
+        jugador.y += 5;
+    }
+}
+/* Creamos la función refresh, para 
+poder cargar los valores por defecto */
+function refresh() {
+    load();
 }
 
-// Creamos la función oMousePos
-function oMousePos(canvas, evt) {
-    /* Obtenemos la posición relativa respecto a la ventana de visualización
-    Y la asignamos a la variable ClienteRect */
-    var ClientRect = canvas.getBoundingClientRect();
-    // Retornamos las posiciones en x, y
-    return {
-        x: Math.round(evt.clientX - ClientRect.left),
-        y: Math.round(evt.clientY - ClientRect.top),
-    };
+function update(evt) {
+    /* Cancela el evento submit, no refresanco la página */
+    evt.preventDefault();
+    /* Inicializamos variables necesarias */
+    var string, nro_personas, k;
+    /* Obtenemos la data enviada en el formulario serializando */
+    string = $(this).serialize().split("&");
+    nro_personas = string[0].split("=");
+    nro_personas = nro_personas[1];
+    k = string[1].split("=");
+    k = k[1];
+    /* Sí el límite de personas es menor o igual
+    al número de personas ingresadas */
+    if (limit_personas >= nro_personas) {
+        /* Limpiamos los inputs */
+        $(this).find("input[name='value_nro_personas']").val("");
+        $(this).find("input[name='value_k']").val("");
+        /* Mostramos una alerta de éxito */
+        alert("Se actualizaron las varibles.");
+        /* Volvemos a cargar la función load con parámetros */
+        load(nro_personas, k);
+    } else {
+        /* Mostramos una alerta de error */
+        alert("El límite del número de personas es de" + limit_personas + ".");
+    }
+}
+/* Creamos la función html_data */
+function html_data(result) {
+    /* Insertamos html en elemento output
+    pasando todas las propiedades obtenidas */
+    document.getElementById("output").innerHTML =
+        `<div class='h5 mt-4 mb-2'>Porcentaje de Infección: <span style="color: red;">` + result.porcentaje + `%</span></div>
+        <div class='h5 mb-2'> Nº de personas total = ` + result.nro_personas + `</div>
+        <div class='h5 mb-2'> k = ` + result.k + `</div>
+        <div class='h5 mb-2'> Nº de personas infectadas = ` + result.nro_infectados + `</div>
+        <div class='h5 mb-2'> Nº de personas sanas = ` + result.nro_sanos + `</div>
+        `;
 }
